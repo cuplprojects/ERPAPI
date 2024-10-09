2f00d18ae44f6caf516cc7782142b41af806efab
cuplprojects <shivom.chandrakala@gmail.com>
2024-10-09 12:06:46 +0530

    Configuration

--- ERPAPI/Data/AppDbContext.cs
+++ ERPAPI/Data/AppDbContext.cs
@@ -18,7 +18,6 @@ namespace ERPAPI.Data
         public DbSet<ProcessGroupType> ProcessGroups { get; set; }
         public DbSet<FeatureEnabling> FeatureEnabling { get; set; }
         public DbSet<Transaction> Transaction { get; set; }
-        //public DbSet<Message> Message { get; set; }
         public DbSet<Camera> Camera { get; set; }
         public DbSet<Alarm> Alarm { get; set; }
         public DbSet<User> Users { get; set; } // Assuming this is already present
@@ -26,8 +25,8 @@ namespace ERPAPI.Data
 
         public DbSet<Role> Role { get; set; }
         public DbSet<Machine> Machine { get; set; } 
-         public DbSet<Zone> Zone { get; set; } 
-
+         public DbSet<Zone> Zone { get; set; }
+        public DbSet<ProjectProcessFeature> ProjectProcessFeatures { get; set; }
         public DbSet<EventLog> EventLogs { get; set; } // Assuming you have event logs
         public DbSet<ErrorLog> ErrorLogs { get; set; } // Assuming you have error logs
 
